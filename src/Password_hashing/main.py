75d93e74d4944f1e0d9f52210d6e95a7dac231de
Hektro754 <ekal2004@gmail.com>
2025-05-12 11:57:26 +0300

    modified to take no salts

--- src/Password_hashing/main.py
+++ src/Password_hashing/main.py
@@ -3,7 +3,7 @@ import hashlib
 
 password = input("Enter your password: ")
 
-hashed = hashlib.pbkdf2_hmac('sha256', password.encode(), 100_000)
+hashed = hashlib.pbkdf2_hmac('sha256', password.encode(), b'', 100_000)
 
 with open('store_password.txt', 'w') as f:
     f.write(f"Hashed password : {hashed.hex()}\n")
--- src/Password_hashing/verify_hash.py
+++ src/Password_hashing/verify_hash.py
@@ -8,7 +8,7 @@ with open(file, "r") as f:
 stored_hash = bytes.fromhex(hash_hex)
 
 user_password = input("Give password : ")
-new_hash = hashlib.pbkdf2_hmac('sha256', user_password.encode(), 100_000)
+new_hash = hashlib.pbkdf2_hmac('sha256', user_password.encode(),b'', 100_000)
 
 if new_hash == stored_hash:
     print("Password is correct")
