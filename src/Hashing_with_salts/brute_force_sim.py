69a76eab61e7d3830d3f7f4191e50a774dcd4073
Hektro754 <ekal2004@gmail.com>
2025-05-12 12:29:19 +0300

    added brute force simulator with random salting

---
+++ src/Hashing_with_salts/brute_force_sim.py
@@ -0,0 +1,26 @@
+import bcrypt
+
+file = "password_storage.txt"
+
+with open(file, 'r') as f:
+    lines = f.readlines()
+    
+    stored_hashed = lines[0].split(": ")[1].strip().encode()
+    stored_salt = lines[1].split(": ")[1].strip().encode()
+    
+possible_password = ["password", "password123", "letmein", "12345qwert", "admin", "user", "user123"]
+
+def brute_force_attack(possible_password):
+    for guess in possible_password:
+        print(f"Trying password : {guess}")
+        hashed_guess = bcrypt.hashpw(guess.encode(), bcrypt.gensalt())
+        if hashed_guess == stored_hashed:
+            print("Password Found")
+            return guess
+        else:
+            print("Password not found")
+    return None
+
+guess = brute_force_attack(possible_password)
+if guess is not None:
+    print(f"The password is : {guess}")
--- src/Hashing_with_salts/password_check.py
+++ src/Hashing_with_salts/password_check.py
@@ -1,9 +1,21 @@
+import bcrypt
 
+def encrypting_passwrd(password,salt):
+    hashed = bcrypt.hashpw(password.encode(), salt)
+    return hashed
 
 file = "password_storage.txt"
 
 with open(file, "r") as f:
     lines = f.readlines()
     
-    stored_hashed = lines[0].split(": ")[1].strip()
-    stored_salt = lines[0].split(": ")[1].strip()
+    stored_hashed = lines[0].split(": ")[1].strip().encode()
+    stored_salt = lines[1].split(": ")[1].strip().encode()
+    
+password = input("Give password : ")
+new_hash = encrypting_passwrd(password,stored_salt)
+
+if new_hash == stored_hashed:
+    print("Password Correct")
+else:
+    print("Password invalid")
