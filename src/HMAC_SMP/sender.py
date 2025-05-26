86ee119623223b41bce6b9c2b812bcde6b050e82
ektoras <ekal2004@gmail.com>
2025-05-26 19:42:14 +0300

    added hmac smp sender

---
+++ src/HMAC_SMP/sender.py
@@ -0,0 +1,12 @@
+import hmac
+import hashlib
+
+SECRET_KEY = b'blablabla blebleble blublublu'
+
+def create_hmac(message : str, key : bytes) -> str:
+    return hmac.new(key, message.encode(), hashlib.sha256).hexdigest()
+
+def send_message(message : str):
+    mac = create_hmac(message, SECRET_KEY)
+    with open('packet.txt', 'x') as p:
+        p.write(f"Message : {message}\nHMAC : {mac}")
